2d77290e8a8e31fa0d3843bc7d8baab9a9f5f26f
NikKha03 <kolyakhalimendik@gmail.com>
2024-02-25 16:21:43 +0300

    add project

---
+++ src/main/java/com/example/springstudents/model/Student.java
@@ -0,0 +1,30 @@
+package com.example.springstudents.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Data
+@Entity
+@Table(name = "students")
+public class Student {
+    public Student() {}
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    @NonNull
+    @Column(unique = true)
+    private String email;
+    @Transient
+    private int age;
+
+    public int getAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+}
